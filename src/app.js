8cb5aa7df11f7a60c09f5b1f6a7d185d8b0b490f
leonardnadeau <leonard.nadeau05@gmail.com>
2025-09-21 21:02:25 -0400

    updatePositions almost done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,47 @@
-function getCoordinates() {
+function updateCoordinates() {
     const { exec } = require('child_process');
     exec('python src/coordinates.py');
 }
 
-getCoordinates()
+const system = ['mercury', 'venus', 'earth', 'moon', 'mars', 'jupiter', 'uranus', 'saturn', 'neptune'];
+
+const fs = require('fs/promises');
+const path = require('path');
+
+async function getCoordinates() {
+  try {
+    const filePath = path.join(__dirname, '../coordinates.json');
+    const jsonString = await fs.readFile(filePath, 'utf8');
+    const data = JSON.parse(jsonString);
+    return data;
+  } catch (err) {
+    console.error('Error loading JSON:', err);
+  }
+}
+
+function updatePositions() {
+    console.log(updateCoordinates());
+    getCoordinates().then(coordinates => {
+        for (body in coordinates) {
+            let [x, y] = transformXYZ(coordinates[body]);
+            let bodyElement = document.getElementById(`#${body}`);
+            bodyElement.style.left = `calc(${x} * 1%)`;
+            bodyElement.style.top = `calc(${y} * 1%)`;
+        }
+    });
+}
+
+function transformXYZ(xyz) {
+    let x = xyz[0];
+    let y = xyz[1];
+    let z = xyz[2];
+
+    let xFraction = x / (x + y);
+    let yFraction = y / (x + y);
+    x += z * xFraction;
+    y += z * yFraction;
+    return [x, y];
+}
+
+
+setInterval(updatePositions, 1000);
